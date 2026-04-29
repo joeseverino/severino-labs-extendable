32b18c0845c1ed66b183ef117d54f384f90c6754
joeseverino <github@jseverino.com>
2026-04-29 08:52:41 -0500

    nav bug fix 5

--- functions.php
+++ functions.php
@@ -16,4 +16,24 @@ add_action('wp_enqueue_scripts', function () {
 
 });
 
-// Test comment
+/**
+ * Mark the active nav item when WordPress doesn't detect it automatically
+ * (e.g. custom links pointing to archives or custom post types).
+ */
+add_action('wp_footer', function () {
+    ?>
+    <script>
+    (function () {
+        var current = window.location.pathname;
+        document.querySelectorAll('.wp-block-navigation a').forEach(function (a) {
+            try {
+                if (new URL(a.href).pathname === current) {
+                    a.closest('.wp-block-navigation-item')
+                     .classList.add('current-menu-item');
+                }
+            } catch (e) {}
+        });
+    })();
+    </script>
+    <?php
+});
